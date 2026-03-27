9d3e025c6515c5210a476f56350758e3f6240e03
stonedDiscord <Tukz@gmx.de>
2026-03-27 09:18:56 +0000

    SCI

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-Bauen
+# Bootloader für MC68332
 
+Empfängt Daten per seriell und schreibt sie in NVRAM
+
+## Bauen :
 `docker run -v .:/opt/m68k_bare_metal/src/ ghcr.io/stoneddiscord/m68k_bare_metal:master make rom`
--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-int
-main(void)
+#include <stdint.h>
+
+volatile uint16_t *SCCR0 = (volatile uint16_t *)0xFFFC08;
+volatile uint16_t *SCCR1 = (volatile uint16_t *)0xFFFC0A;
+volatile uint16_t *SCSR  = (volatile uint16_t *)0xFFFC0C;
+volatile uint16_t *SCDR  = (volatile uint16_t *)0xFFFC0E;
+
+int main(void)
 {
     return 0;
 }
