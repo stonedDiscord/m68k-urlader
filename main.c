fd98dbc0bdc109bd8c637f7bd6a06cd2f1959fe6
stonedDiscord <Tukz@gmx.de>
2026-03-27 15:51:33 +0100

    putchar

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdint.h>
 
+volatile uint16_t *SYNCR = (volatile uint16_t *)0xFFFA04;
+
 volatile uint16_t *SCCR0 = (volatile uint16_t *)0xFFFC08;
 volatile uint16_t *SCCR1 = (volatile uint16_t *)0xFFFC0A;
 volatile uint16_t *SCSR  = (volatile uint16_t *)0xFFFC0C;
-volatile uint16_t *SCDR  = (volatile uint16_t *)0xFFFC0E;
+
+volatile uint8_t *SCDR  = (volatile uint8_t *)0xFFFC0F;
+
+void putchar_(char c)
+{
+    while ((*SCSR & 1) == 0)
+        ;
+    *SCDR = c;
+}
 
 int main(void)
 {
+    *SYNCR = 0x7f05u;
+    *SCCR0 = 0x0009u;
+    
+    printf("Pimmel!\n");
+
     return 0;
 }
